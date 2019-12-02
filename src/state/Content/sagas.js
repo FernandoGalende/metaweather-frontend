0eecbbaafb62338933db7bb4b3797f7be0c11457
FernandoGalende <fernandogalende@gmail.com>
2019-12-02 21:19:04 +0100

    Bug fixed

--- src/state/Content/sagas.js
+++ src/state/Content/sagas.js
@@ -10,22 +10,21 @@ function* getWeatherSaga({payload}) {
 	try {
 		yield put(showLoading());
 		const cityPayload = yield contentAPI.getCode(city);
-		const cityCode = cityPayload[0] && cityPayload[0].woeid ? cityPayload[0].woeid : null;
+		const cityCode = cityPayload.data[0] && cityPayload.data[0].woeid ? cityPayload.data[0].woeid : null;
 		if (cityCode) {
-			let result = yield contentAPI.getWheather(cityCode);
-			result = {
-				city: result.title,
-				weather: result.consolidated_weather[0].weather_state_name,
-				temperature: Math.round(result.consolidated_weather[0].the_temp),
-				img: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[0]
+			let {data} = yield contentAPI.getWheather(cityCode);
+			data = {
+				city: data.title,
+				weather: data.consolidated_weather[0].weather_state_name,
+				temperature: Math.round(data.consolidated_weather[0].the_temp),
+				img: `https://www.metaweather.com/static/img/weather/${data.consolidated_weather[0]
 					.weather_state_abbr}.svg`,
 			};
-			yield put(contentActions.setWeather(result));
+			yield put(contentActions.setWeather(data));
 		} else {
 			yield put(contentActions.setErrorMessage('Invalid city name'));
 		}
 	} catch (error) {
-		console.log('hola catch', error.stack);
 		handlingError(error);
 	} finally {
 		yield put(hideLoading());
