e9dd37a27e85eaafecf856064f22a8c8cbece0ba
FernandoGalende <fernandogalende@gmail.com>
2019-12-02 13:02:06 +0100

    filters style and average funcionality

--- src/components/commons/Dropdown/Dropdown.component.js
+++ src/components/commons/Dropdown/Dropdown.component.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import style from './Dropdown.module.scss';
 
-const Dropdown = ({ items = [], itemSelected, defaultSelected }) => (
+const items = [{value: 'ascending'}, {value: 'descending'}, {value: 'none'}];
+
+const Dropdown = ({defaultSelected, itemSelected}) => (
 	<div>
 		<select
 			className={style.dropdown}
@@ -11,20 +14,30 @@ const Dropdown = ({ items = [], itemSelected, defaultSelected }) => (
 		>
 			{defaultSelected ? <option>{defaultSelected}</option> : ''}
 			{defaultSelected ? (
-				items.filter((item) => item.name !== defaultSelected).map((item) => (
-					<option key={item.name} value={item.value}>
-						{item.name}
+				items.filter((item) => item.value !== defaultSelected).map((item) => (
+					<option key={item.value} value={item.value}>
+						{item.value}
 					</option>
 				))
 			) : (
 				items.map((item) => (
-					<option key={item.name} value={item.value}>
-						{item.name}
+					<option key={item.value} value={item.value}>
+						{item.value}
 					</option>
 				))
 			)}
 		</select>
 	</div>
 );
+
+Dropdown.defaultProps = {
+	cidefaultSelectedties: [],
+	itemSelected: () => 0,
+};
+
+Dropdown.propTypes = {
+	defaultSelected: PropTypes.string.isRequired,
+	itemSelected: PropTypes.func.isRequired,
+};
 
 export default Dropdown;
--- src/components/pages/JSChallenge/JSChallenge.component.js
+++ src/components/pages/JSChallenge/JSChallenge.component.js
@@ -2,52 +2,70 @@ import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Dropdown from '../../commons/Dropdown/Dropdown.component';
 import WeatherCard from '../../commons/WeatherCard/WeatherCard.component';
-
 import style from './JSChallenge.module.scss';
 
 const JSChallenge = ({getWeather, cities}) => {
 	const [city, setCity] = useState();
 	const [myCities, setMyCities] = useState([...cities]);
+	const [mainInput, setMainInput] = useState('');
+	const [average, setAverage] = useState(0);
 
 	useEffect(
 		() => {
+			if (cities.length > 0) {
+				setAverageTemperature(cities);
+			}
 			setMyCities(cities);
 		},
-		[cities]
+		[average, cities],
 	);
+
+	const setAverageTemperature = (cityList) => {
+		setAverage(cityList.reduce((total, item) => total + item.temperature, 0) / cityList.length);
+	};
 
 	const onClickSearch = (event) => {
 		event.preventDefault();
+		event.target.value = '';
+		mainInput.value = '';
 		getWeather(city);
 	};
 
-	const oderBy = [{name: '-', value: '-'}, {name: 'up', value: 'up'}, {name: 'down', value: 'down'}];
-
 	const sortTemperture = (order) => {
 		const sortedCities = [...cities];
-		if (order === 'up') {
+		if (order === 'ascending') {
 			setMyCities(sortedCities.sort((a, b) => a.temperature - b.temperature));
-		} else if (order === 'down') {
+		} else if (order === 'descending') {
 			setMyCities(sortedCities.sort((a, b) => b.temperature - a.temperature));
 		}
 	};
 
 	return (
 		<div>
-			<span>Order temperature by: </span>
-			<Dropdown items={oderBy} itemSelected={sortTemperture} selectedNamespace={'-'} />
-			<div className={style.formContainter}>
-				<form onSubmit={onClickSearch}>
-					<h5>Search City </h5>
-					<div>
-						<input onChange={(evt) => setCity(evt.target.value)} />
-					</div>
-					<button onClick={onClickSearch}>Search</button>
-				</form>
+			<div className={style.containter}>
+				<div className={style.searchBlock}>
+					<form onSubmit={onClickSearch}>
+						<input
+							ref={(ref) => setMainInput(ref)}
+							placeholder="Add city by name"
+							onChange={(evt) => setCity(evt.target.value)}
+						/>
+						<button onClick={onClickSearch}>Search</button>
+					</form>
+				</div>
+				<div className={style.orderByBlock}>
+					<span>Order temp by: </span>
+					<Dropdown defaultSelected={'none'} itemSelected={sortTemperture} />
+				</div>
+				<div className={style.averageByBlock}>
+					<span>
+						Average temp. <strong>{average}º</strong>
+					</span>
+				</div>
 			</div>
 			<div className={style.resultContainer}>
-				{myCities &&
-					myCities.map((element) => (
+				{myCities
+					&& myCities.map((element) => (
 						<WeatherCard
 							key={element.city}
 							city={element.city}
