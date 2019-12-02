a4af28ab8194a642adef550ce145834b28f7bb05
FernandoGalende <fernandogalende@gmail.com>
2019-12-02 23:30:51 +0100

    Home page minimun details

--- src/components/pages/Home/Home.component.js
+++ src/components/pages/Home/Home.component.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import style from './Home.module.scss';
 
-const Home = (props) => {
-	return (
-		<div className={style.cardContainer}>
-			<Link to={'/layoutchalenge'}>layout</Link>
+const Home = (props) => (
+	<div className={style.container}>
+		<p className={`${style.line} ${style.typewriter}`}>
+			Hi there! This is <strong>Fernando Galende</strong>, please choose one option, and let's start!
+		</p>
+		<div className={`${style.linkContainer}`}>
+			<Link to={'/layoutchalenge'}>Layout Challenge</Link>
 			<Link to={'/jschallenge'}>JavaScript Challenge</Link>
 		</div>
-	);
-};
+	</div>
+);
 
 export default Home;
